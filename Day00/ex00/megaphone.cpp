a234b11665a9a85a0bfe01cb6a7f2154aad04abb
Rel-hach <rachidelhachimi6@gmail.com>
2022-11-21 04:35:26 +0100

    finish: finish ex00

--- Day00/ex00/megaphone.cpp
+++ Day00/ex00/megaphone.cpp
@@ -6,28 +6,31 @@
 /*   By: rel-hach <rel-hach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 20:18:35 by rel-hach          #+#    #+#             */
-/*   Updated: 2022/11/19 01:17:49 by rel-hach         ###   ########.fr       */
+/*   Updated: 2022/11/19 01:35:49 by rel-hach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int main(int ac, char **av)
+int main (int ac, char **av)
 {
-    int i = 0;
     char    c;
+
     if (ac == 1)
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-    while (av[++i])
+    else
     {
-        int j = -1;
-        while (av[i][++j])
+        for (int i = 1; i < ac; i++)
         {
-            c = toupper(av[i][j]);
-            std::cout << c;
+            std::string str = av[i];
+            for (int j = 0; j < (int)str.size(); j++)
+            {
+                c = std::toupper(str[j]);
+                std::cout << c;
+            }
+            if (i != ac - 1)
+                std::cout << " ";
         }
-        if (av[i + 1])
-            std::cout << " ";
     }
     std::cout << std::endl;
 }
