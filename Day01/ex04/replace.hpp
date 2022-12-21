3395a62ec77fd6946dcb8da7b72e9cdcf584fc1a
Rel-hach <rachidelhachimi6@gmail.com>
2022-12-22 00:16:28 +0100

    feat: create Headerfile, add needed libs & funcs

---
+++ Day01/ex04/replace.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   replace.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rel-hach <rel-hach@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/21 23:30:17 by rel-hach          #+#    #+#             */
+/*   Updated: 2022/12/21 23:33:41 by rel-hach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef REPLACE_HPP
+#define REPLACE_HPP
+
+#include <iostream>
+#include <fstream>
+
+void    putError(std::string errMsg);
+
+#endif
