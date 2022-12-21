2b5f085c94597f98dbb4ad3fb42ff4ee1b18eaba
Rel-hach <rachidelhachimi6@gmail.com>
2022-12-22 00:18:47 +0100

    fix: fix the problem of entering the same name

--- Day01/ex04/main.cpp
+++ Day01/ex04/main.cpp
@@ -6,46 +6,48 @@
 /*   By: rel-hach <rel-hach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/19 17:12:49 by rel-hach          #+#    #+#             */
-/*   Updated: 2022/12/20 23:52:11 by rel-hach         ###   ########.fr       */
+/*   Updated: 2022/12/22 00:00:32 by rel-hach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <fstream>
+#include "replace.hpp"
 
 int main (int ac, char **av)
 {
     if (ac == 4)
     {
-        std::string file_name = av[1];
+        std::string infile_name = av[1];
         std::string s1 = av[2];
         std::string s2 = av[3];
         std::string line;
-        std::ifstream file;
+        std::ifstream inputStream;
         std::string outfile;
         int ret;
-        file.open(file_name);
-        if (file.fail())
-            std::cout << "error" << std::endl;
-        outfile = file_name + ".replace";
-        std::ofstream o;
-        o.open(outfile);
-        if (o.fail())
-            std::cout << "error" << std::endl;
-        while (std::getline(file, line))
+        int i = 0;
+        inputStream.open(infile_name);
+        if (inputStream.fail())
+            putError("input file couldn't be opened");
+        outfile = infile_name + ".replace";
+        std::ofstream outputStream;
+        outputStream.open(outfile);
+        if (outputStream.fail())
+            putError("input file couldn't be opened");
+        while (std::getline(inputStream, line))
         {
             ret = line.find(s1);
-            while (ret != -1)
+            while (ret != -1 && line[i] != '\0')
             {
                 line.erase(ret, s1.size());
                 line.insert(ret, s2);
                 ret = line.find(s1);
+                i = i + s1.size();
             }
-            o << line;
-            if (!file.eof())
-                o << std::endl;
+            outputStream << line;
+            if (!inputStream.eof())
+                outputStream << std::endl;
         }
-        file.close();
+        inputStream.close();
+        outputStream.close();
     }
     return (0);
 }
