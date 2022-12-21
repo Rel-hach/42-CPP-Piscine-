afd9efd368203562874ff507875320660c5d6865
Rel-hach <rachidelhachimi6@gmail.com>
2022-12-22 00:25:24 +0100

    refactor: protect new in case of failure w nothrow

--- Day01/ex00/newZombie.cpp
+++ Day01/ex00/newZombie.cpp
@@ -6,7 +6,7 @@
 /*   By: rel-hach <rel-hach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 22:59:56 by rel-hach          #+#    #+#             */
-/*   Updated: 2022/11/29 02:24:54 by rel-hach         ###   ########.fr       */
+/*   Updated: 2022/12/21 23:27:21 by rel-hach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 
 Zombie* newZombie( std::string name )
 {
-    Zombie  * obj = new Zombie(name);
+    Zombie  * obj = new (std::nothrow)Zombie(name);
+    if (!obj)
+    {
+        std::cout << "Allocation Failed .. " << std::endl;
+        exit (EXIT_FAILURE);
+    }
     return (obj);
 }
