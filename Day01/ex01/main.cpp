120ea6e00a1986c4d715708fbdd6e061e7b79ba2
Rel-hach <rachidelhachimi6@gmail.com>
2022-12-21 02:47:33 +0100

    Test: create main function, test code.

--- Day01/ex01/main.cpp
+++ Day01/ex01/main.cpp
@@ -6,8 +6,21 @@
 /*   By: rel-hach <rel-hach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 23:25:40 by rel-hach          #+#    #+#             */
-/*   Updated: 2022/11/24 23:29:12 by rel-hach         ###   ########.fr       */
+/*   Updated: 2022/12/21 02:35:30 by rel-hach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+
+int main ()
+{
+    Zombie *zombie = zombieHorde( 10, "Zombie" );
+    int i = 0;
+    while (i < 10)
+    {
+        zombie[i].announce();
+        i++;
+    }
+    delete [] zombie;
+    return 0;
+}
