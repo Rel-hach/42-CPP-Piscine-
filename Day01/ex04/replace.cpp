1c4d4fa8918d041f8c41e62cdff2a09b31c3f65a
Rel-hach <rachidelhachimi6@gmail.com>
2022-12-22 00:15:08 +0100

    feat: add putError function

--- Day01/ex04/replace.cpp
+++ Day01/ex04/replace.cpp
@@ -6,7 +6,14 @@
 /*   By: rel-hach <rel-hach@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 00:19:10 by rel-hach          #+#    #+#             */
-/*   Updated: 2022/12/20 00:19:11 by rel-hach         ###   ########.fr       */
+/*   Updated: 2022/12/21 23:33:55 by rel-hach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "replace.hpp"
+
+void    putError(std::string errMsg)
+{
+    std::cout << errMsg << std::endl;
+    exit(EXIT_FAILURE);
+}
