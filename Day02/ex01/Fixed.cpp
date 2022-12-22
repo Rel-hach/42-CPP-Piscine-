3fc912f216b84ce496bf5bcd4b941be7d6c980fb
Rel-hach <rachidelhachimi6@gmail.com>
2022-12-22 06:08:39 +0100

    first commit

---
+++ Day02/ex01/Fixed.cpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rel-hach <rel-hach@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/22 05:55:50 by rel-hach          #+#    #+#             */
+/*   Updated: 2022/12/22 06:02:46 by rel-hach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+    
+}
+
+Fixed::Fixed(const Fixed &cp)
+{
+    
+}
+
+Fixed::~Fixed()
+{
+    
+}
+
+Fixed &Fixed:: operator=(const Fixed &cp)
+{
+
+}
+
+int Fixed::getRawBits( void ) const
+{
+
+}
+
+void Fixed::setRawBits( int const raw )
+{
+    
+}
+
+float Fixed::toFloat( void ) const
+{
+    
+}
+
+int Fixed::toInt( void ) const
+{
+    
+}
