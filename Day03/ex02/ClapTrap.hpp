bf644915e162520f49f7a968e3b5945c09638ce7
rel-hach <rachidelhachimi6@gmail.com>
2023-03-13 22:49:52 +0100

    feat : add CT headerfile

---
+++ Day03/ex02/ClapTrap.hpp
@@ -0,0 +1,26 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap
+{
+    protected:
+        std::string         _name;
+        unsigned int        _HitPoint;
+        unsigned int        _EnergyPoint;
+        unsigned int        _Damage;
+    public:
+        ClapTrap();
+        virtual ~ClapTrap();
+        ClapTrap(std::string name);
+        ClapTrap(ClapTrap const &cp);
+        void attack(const std::string& target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
+        std::string getName() const;
+        unsigned int getDamage() const;
+        ClapTrap& operator =( const ClapTrap &r );
+};
+
+#endif
